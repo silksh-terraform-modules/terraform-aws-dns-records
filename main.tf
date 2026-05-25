f0b839b8eae850a3ae6966fd5f9943399ff1009a
Marcin Franikowski <marcin.franikowski@silksh.pl>
2026-05-25 09:25:18 +0200

    Add .gitignore and refactor Route53 record handling
    - Introduced .gitignore to exclude .DS_Store files.
    - Refactored Route53 record resource to use local variable for better management of records, improving readability and maintainability.

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
 data "aws_route53_zone" "zone" {
-  name         = var.tld
-  vpc_id       = var.vpc_id
+  name   = var.tld
+  vpc_id = var.vpc_id
+}
+
+locals {
+  route53_records = {
+    for key, record in var.records : "${key}|${record.type}" => {
+      name    = endswith(key, "|${record.type}") ? trimsuffix(key, "|${record.type}") : key
+      type    = record.type
+      ttl     = try(record.ttl, "60")
+      records = record.value
+    }
+  }
 }
 
 resource "aws_route53_record" "records" {
-
-  for_each = var.records
+  for_each = local.route53_records
   zone_id  = data.aws_route53_zone.zone.id
-  name     = "${regex("^.", each.key) != "@" ? "${each.key}." : ""}${var.tld}."
+  name     = "${regex("^.", each.value.name) != "@" ? "${each.value.name}." : ""}${var.tld}."
   type     = each.value.type
-  ttl      = try(each.value.ttl, "60")
-  records  = each.value.value
-
+  ttl      = each.value.ttl
+  records  = each.value.records
 }
